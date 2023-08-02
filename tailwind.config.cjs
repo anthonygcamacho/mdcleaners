6f7f65ea8fef222bf6984ce25ea1de99c2d66b57
Anthony Camacho <anthony.g.camacho@gmail.com>
2023-08-02 13:32:17 -0700

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx,css}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
