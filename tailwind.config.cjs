17ab05bf9d5e2c1427165ec9e72660c7a79bbdb8
Anthony Camacho <anthony.g.camacho@gmail.com>
2023-08-11 10:27:40 -0700

    building

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: "class",
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx,css}"],
     theme: {
+        // fontFamily: {
+        //     sans: ["Roboto", "sans-serif"],
+        //     body: ["Roboto", "sans-serif"],
+        //     mono: ["ui-monospace", "monospace"],
+        // },
         extend: {},
     },
     plugins: [],
+    corePlugins: {
+        preflight: true,
+    },
 }
